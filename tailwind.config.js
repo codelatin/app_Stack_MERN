a2d44587d9d2d89beff24ce377803b61318b9f21
codelatin <codelatincolombia@gmail.com>
2024-07-03 12:20:23 -0500

    commit1

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html', // Asegúrate de incluir el archivo HTML de tu aplicación
+    './src/**/*.{js,jsx,ts,tsx}', // Incluye todos los archivos de React en la carpeta src
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: ["valentine"],
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
